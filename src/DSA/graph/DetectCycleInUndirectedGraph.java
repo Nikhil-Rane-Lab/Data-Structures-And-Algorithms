2463dffe06cd9da9f1f38fa2e1f5d98b7b03f7cd
Nikhil R <ranenikhil999@gmail.com>
2024-08-19 09:55:45 +0530

    dsa

---
+++ src/DSA/graph/DetectCycleInUndirectedGraph.java
@@ -0,0 +1,42 @@
+package DSA.graph;
+
+import java.util.ArrayList;
+
+//https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph
+public class DetectCycleInUndirectedGraph {
+//https://www.youtube.com/watch?v=zQ3zgFypzX4&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=12&pp=iAQB
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, -1, visited, adj)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+
+        visited[node] = true;
+
+        for (Integer neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, node, visited, adj)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //TC: O(V+E)
+    //SC: O(V)
+}
--- src/DSA/graph/L210CourseSchedule2.java
+++ src/DSA/graph/L210CourseSchedule2.java
@@ -1,9 +1,6 @@
 package DSA.graph;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 //https://leetcode.com/problems/course-schedule-ii
 public class L210CourseSchedule2 {
