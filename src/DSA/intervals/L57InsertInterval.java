5c854c9cf22b48697c9c9c4a0965442ae889a2aa
Nikhil R <ranenikhil999@gmail.com>
2024-09-15 11:45:57 +0530

    dsa

--- src/DSA/intervals/L57InsertInterval.java
+++ src/DSA/intervals/L57InsertInterval.java
@@ -9,14 +9,16 @@ public class L57InsertInterval {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
         List<int[]> result = new ArrayList<>();
-        int i = 0;
+        int i = 0; //⭐same i is used till last loop without being reset
         int n = intervals.length;
 
+        // Step 1: Add all intervals that come before newInterval
         while (i < n && intervals[i][1] < newInterval[0]) {
             result.add(intervals[i]);
             i++;
         }
 
+        // Step 2: Merge overlapping intervals
         while (i < n && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
@@ -24,6 +26,7 @@ public class L57InsertInterval {
         }
         result.add(newInterval);
 
+        // Step 3: Add all intervals that start after newInterval ends
         while (i < n) {
             result.add(intervals[i]);
             i++;
