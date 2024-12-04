8a7297981483652fdbdf18ff5fbc6de57ea48ccc
Nikhil R <ranenikhil999@gmail.com>
2024-12-04 07:15:44 +0530

    dsa

--- src/DSA/graph/GFGDetectCycleInUndirectedGraph.java
+++ src/DSA/graph/GFGDetectCycleInUndirectedGraph.java
@@ -2,7 +2,7 @@ package DSA.graph;
 
 import java.util.ArrayList;
 
-//https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph
+//https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 public class GFGDetectCycleInUndirectedGraph {
 //https://www.youtube.com/watch?v=zQ3zgFypzX4&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=12&pp=iAQB
 
@@ -11,7 +11,7 @@ public class GFGDetectCycleInUndirectedGraph {
         boolean[] visited = new boolean[V];
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
-                if (dfs(i, -1, visited, adj)) {
+                if (isCycleExistsByDFS(i, -1, visited, adj)) { // ⚠️ parent is passed as any value say -1
                     return true;
                 }
             }
@@ -20,23 +20,24 @@ public class GFGDetectCycleInUndirectedGraph {
         return false;
     }
 
-    private boolean dfs(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+    private boolean isCycleExistsByDFS(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
 
         visited[node] = true;
 
         for (Integer neighbor : adj.get(node)) {
             if (!visited[neighbor]) {
-                if (dfs(neighbor, node, visited, adj)) {
+                if (isCycleExistsByDFS(neighbor, node, visited, adj)) {
                     return true;
                 }
-            } else if (neighbor != parent) {
+            } else if (neighbor != parent) { // If the neighbor is visited and not the parent, we found a cycle
                 return true;
             }
+            // ⚠️ there is return true in both if block and else block
         }
 
         return false;
     }
 
-    //TC: O(V+E)
-    //SC: O(V)
+    //TC: O(V+E) - Each vertex and edge is processed once in DFS, where V is the number of vertices and  E is the number of edges.
+    //SC: O(V) - We need extra space for the visited array and the recursive call stack in DFS.
 }
--- src/DSA/graph/GFGFloodFillAlgorithm.java
+++ src/DSA/graph/GFGFloodFillAlgorithm.java
@@ -1,25 +1,29 @@
 package DSA.graph;
 
+// https://www.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
 public class GFGFloodFillAlgorithm {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int initialColor = image[sr][sc];
+
+        // If the original color is the same as the new color, no filling is needed
         if (initialColor == newColor) {
             return image;
         }
 
-        dfs(image, sr, sc, newColor, initialColor);
+        dfs(image, sr, sc, newColor, initialColor); // Perform flood fill starting from (sr, sc)
         return image;
     }
 
     private void dfs(int[][] image, int sr, int sc, int newColor, int initialColor) {
+        // Base case: if out of bounds or not the initial color
         if (sr < 0 || sr >= image.length
                 || sc < 0 || sc >= image[0].length
-                || image[sr][sc] != initialColor) {
+                || image[sr][sc] != initialColor) { // ⚠️
             return;
         }
 
-        image[sr][sc] = newColor;
+        image[sr][sc] = newColor; // Change the color of the current pixel
 
         dfs(image, sr - 1, sc, newColor, initialColor); //up
         dfs(image, sr + 1, sc, newColor, initialColor); //down
@@ -27,6 +31,7 @@ public class GFGFloodFillAlgorithm {
         dfs(image, sr, sc + 1, newColor, initialColor); //right
     }
 }
-    //TC: O(n*m) where n is the number of rows and m is the number of columns.
-    // In the worst case, all pixels need to be visited.
-    //SC: O(n*m)
+// TC: O(N * M) where N is the number of rows and M is the number of columns.
+// In the worst case, all pixels need to be visited.
+// SC: O(N * M) which is due to the recursion stack for the DFS call (or a queue if using BFS). In the worst case,
+// if the image is entirely filled with the starting color, the recursion stack could reach N * M depth.
